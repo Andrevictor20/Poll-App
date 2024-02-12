808673d2e91f05e553f634b45b6665c3e4b9b26b
André Victor <victorandre280@gmail.com>
2024-02-11 22:32:08 -0300

    Adição do uso de websocket e Redis

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import cookie from "@fastify/cookie";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
@@ -10,9 +12,11 @@ app.register(cookie, {
   secret: "polls-app-secret",
   hook: "onRequest",
 });
+app.register(websocket)
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll); 
+app.register(pollResults)
 
 app
   .listen({
